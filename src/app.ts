179995be9f06a52359348aebbbbbc3da91215b4c
akul <nurislamimanudin@gmail.com>
2025-09-10 13:54:49 +0700

    register group into app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { authenticatePlayer } from './middlewares/authentication';
 import playerController from './modules/player/player.controller';
+import groupController from './modules/group/group.controller';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(authenticatePlayer);
 
 // Routes
 app.use('/players', playerController);
+app.use('/groups', groupController);
 
 export default app;
