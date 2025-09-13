e6156bff8dde3c1fbe4dc46f1267ea0cac27ca0d
akul <nurislamimanudin@gmail.com>
2025-09-13 18:59:14 +0700

    register battles controller

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { errorHandler } from './error-handler';
 import { authenticatePlayer } from './middlewares/authentication';
-import playerController from './modules/player/player.controller';
+import battleController from './modules/battle/battle.controller';
 import groupController from './modules/group/group.controller';
+import playerController from './modules/player/player.controller';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(authenticatePlayer);
 // Routes
 app.use('/players', playerController);
 app.use('/groups', groupController);
+app.use('/battles', battleController);
 
 app.use(errorHandler);
 
