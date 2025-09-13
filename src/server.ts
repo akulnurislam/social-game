b0ca158cafbe96ee9a87b3b6ba9054d0abeb4903
akul <nurislamimanudin@gmail.com>
2025-09-13 18:55:40 +0700

    update api-server

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ const PORT = process.env['API_PORT'] || 3000;
 
 export function startApiServer() {
   app.listen(PORT, () => {
-    console.log(`API server running on http://localhost:${PORT}`);
+    console.log(`[API Server] running on http://localhost:${PORT}`);
   });
 }
